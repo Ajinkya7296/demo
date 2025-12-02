dcd6db51f6613da82fdc2994ec8928e3369582ff
Ajinkya7296 <ajinkyabansode123@gmail.com>
2025-12-02 13:19:32 +0530

    this is new commit

---
+++ src/test/java/sample/test.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		String name= "ajinkya";
+		
+		if(name=="ajinkya") {
+			System.out.println("project is successful");
+		} else {
+			System.out.println("project is not successful");
+		}
+
+	}
+
+}
